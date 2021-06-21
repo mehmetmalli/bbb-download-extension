534de1325245f008bfaa5fa47317a82c49c59c06
Mehmet MALLI <mallimehmet@gmail.com>
2021-06-21 18:15:52 +0300

    v2 started from template, functionality done, needs polishing

--- v2/index.js
+++ v2/index.js
@@ -1,19 +1,10 @@
 (function() {
-  if (window.hasRun) {
-    return;
-  }
-  window.hasRun = true;
 
-  browser.runtime.onMessage.addListener((message) => {
-    if (message.command === "download") {
       document.querySelectorAll("video").forEach(n => {
         n.childNodes.forEach( c => {
-          console.log(c);
           open(window.location.origin + c.attributes.src.value);
         })
       })
-    }
-  });
 
   function open(url) {
     fetch(url).then( res => {
